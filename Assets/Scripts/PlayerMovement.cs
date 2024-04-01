2d3787e058af031d9b97b080b39dc005436a8e80
HarmonySeeker <43490370+HarmonySeeker@users.noreply.github.com>
2024-04-01 23:19:49 +0700

    added crosshair and weapon to player (WIP)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int speed = 5;
+    [SerializeField] private float health = 100.0f;
+
+    [SerializeField] private Transform weapon;
+    [SerializeField] private GameObject crosshair;
+
+    [SerializeField] private float crosshairDistance = 1.0f;
+    [SerializeField] private float minimumCrosshairDistance = 1.0f;
 
     private Vector2 movement;
     private Rigidbody2D rb;
@@ -17,10 +25,15 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+
+    }
+
     private void OnMovement(InputValue value)
     {
         movement = value.Get<Vector2>();
-
+        
         if (movement.x != 0 || movement.y != 0)
         {
             animator.SetFloat("X", movement.x);
@@ -35,5 +48,27 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        
+        if (movement != Vector2.zero)
+        {
+            crosshair.transform.localPosition = movement * crosshairDistance;
+            weapon.transform.right = crosshair.transform.position - weapon.position;
+        }
+
+    }
+
+    public void GetShot(float incDmg)
+    {
+        health = health > incDmg ? health - incDmg : 0;
+
+        if (health == 0)
+        {
+            this.die();
+        }
+    }
+
+    private void die()
+    {
+        this.gameObject.SetActive(false);
     }
 }
